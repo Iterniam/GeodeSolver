1b68736e41b2b8ed275c5bcc16ac87a1f8f3b978
Iterniam <24490028+Iterniam@users.noreply.github.com>
2022-10-01 14:05:35 +0200

    Add range to flying machine footprints

--- src/Enums/flying_machine_enum.py
+++ src/Enums/flying_machine_enum.py
@@ -1,8 +1,31 @@
 from enum import Enum
 from types import MappingProxyType
+from typing import SupportsIndex
 
 from src.Enums.axis_enum import Axis
 from src.Enums.data_annotations import DataPrimitive
+
+
+class Range:
+
+    def __init__(self,
+                 start: SupportsIndex,
+                 stop: SupportsIndex,
+                 step: SupportsIndex = None,
+                 exceptions: set[int] = None):
+        self._range = range(start, stop, 1 if step is None else step)
+        # Only store the exceptions that are part of the range
+        self._exceptions = (set() if exceptions is None else
+                            {exception for exception in exceptions if exception in self._range})
+
+    def __contains__(self, item):
+        return item in self._range and item not in self._exceptions
+
+    def __iter__(self):
+        return (number for number in self._range if number not in self._exceptions)
+
+    def __len__(self):
+        return len(self._range) - len(self._exceptions)
 
 
 class _FlyingMachineDP(DataPrimitive):
@@ -16,11 +39,11 @@ class _FlyingMachineDP(DataPrimitive):
             length: int,
             trigger_delay: int,
             engine_footprint: list[list[bool]],
-            pushed_blocks_footprints: dict[int, list[list[bool]]] = None,
+            pushed_blocks_footprints: dict[int, list[list[Range]]] = None,
             pushed_blocks: dict[int, int] = None,
-            attached_blocks_footprints: dict[int, list[list[bool]]] = None,
+            attached_blocks_footprints: dict[int, list[list[Range]]] = None,
             attached_blocks: dict[int, int] = None,
-            pulled_blocks_footprints: dict[int, list[list[bool]]] = None,
+            pulled_blocks_footprints: dict[int, list[list[Range]]] = None,
             pulled_blocks: dict[int, int] = None):
         return ()
 
@@ -29,9 +52,9 @@ class FlyingMachineEnum(Enum):
     pushed_blocks: MappingProxyType[int, int]
     attached_blocks: MappingProxyType[int, int]
     pulled_blocks: MappingProxyType[int, int]
-    pushed_blocks_footprints: MappingProxyType[int, tuple[tuple[bool, ...], ...]]
-    attached_blocks_footprints: MappingProxyType[int, tuple[tuple[bool, ...], ...]]
-    pulled_blocks_footprints: MappingProxyType[int, tuple[tuple[bool, ...], ...]]
+    pushed_blocks_footprints: MappingProxyType[int, tuple[tuple[Range, ...], ...]]
+    attached_blocks_footprints: MappingProxyType[int, tuple[tuple[Range, ...], ...]]
+    pulled_blocks_footprints: MappingProxyType[int, tuple[tuple[Range, ...], ...]]
 
     @_FlyingMachineDP
     def __new__(cls,
@@ -43,11 +66,11 @@ class FlyingMachineEnum(Enum):
                 length: int,
                 trigger_delay: int,  # Game ticks, not redstone ticks
                 engine_footprint: list[list[bool]],
-                pushed_blocks_footprints: dict[int, list[list[bool]]] = None,
-                pushed_blocks: dict[int, int] = None,
-                attached_blocks_footprints: dict[int, list[list[bool]]] = None,
+                pushed_blocks_footprints: dict[int, list[list[Range]]] = None,
+                pushed_blocks: dict[int, int] = None,  # Count excludes the block covered by the pushing piston
+                attached_blocks_footprints: dict[int, list[list[Range]]] = None,
                 attached_blocks: dict[int, int] = None,
-                pulled_blocks_footprints: dict[int, list[list[bool]]] = None,
+                pulled_blocks_footprints: dict[int, list[list[Range]]] = None,
                 pulled_blocks: dict[int, int] = None):
 
         obj = object.__new__(cls)
@@ -67,13 +90,13 @@ class FlyingMachineEnum(Enum):
         obj.pulled_blocks = (MappingProxyType({}) if pulled_blocks is None
                              else MappingProxyType({key: val for key, val in pulled_blocks.items()}))
         obj.pushed_blocks_footprints = (MappingProxyType({}) if pushed_blocks_footprints is None
-                                        else MappingProxyType({key: tuple(tuple(bool_list) for bool_list in val)
+                                        else MappingProxyType({key: tuple(tuple(range_list) for range_list in val)
                                                                for key, val in pushed_blocks_footprints.items()}))
         obj.attached_blocks_footprints = (MappingProxyType({}) if attached_blocks_footprints is None
-                                          else MappingProxyType({key: tuple(tuple(bool_list) for bool_list in val)
+                                          else MappingProxyType({key: tuple(tuple(range_list) for range_list in val)
                                                                  for key, val in attached_blocks_footprints.items()}))
         obj.pulled_blocks_footprints = (MappingProxyType({}) if pulled_blocks_footprints is None
-                                        else MappingProxyType({key: tuple(tuple(bool_list) for bool_list in val)
+                                        else MappingProxyType({key: tuple(tuple(range_list) for range_list in val)
                                                                for key, val in pulled_blocks_footprints.items()}))
 
         return obj
@@ -86,21 +109,8 @@ class FlyingMachineEnum(Enum):
         length=8,
         trigger_delay=0,
         engine_footprint=[[True, True]],
-        pushed_blocks_footprints={1: [[True, True]]},
-        pushed_blocks={1: 2},
-        attached_blocks_footprints={1: [[False, True]]},
-        attached_blocks={1: 6},
-    )
-    MANGO_MACHINE_ATTACHED = _FlyingMachineDP.new(
-        name='MangoMachineAttached',
-        axes=[Axis.Horizontal, Axis.Vertical],
-        uses_qc=False,
-        tileable=True,
-        length=8,
-        trigger_delay=0,
-        engine_footprint=[[True, True]],
-        attached_blocks_footprints={1: [[True, False]],
-                                    2: [[False, True]]},
+        attached_blocks_footprints={1: [[Range(0, 5), Range(0, 1)]],
+                                    2: [[Range(0, 0), Range(5, 8)]]},
         attached_blocks={1: 2, 2: 6},
     )
     L_SHAPE_DOUBLE_PUSHER = _FlyingMachineDP.new(
@@ -112,15 +122,15 @@ class FlyingMachineEnum(Enum):
         trigger_delay=0,
         engine_footprint=[[True, True],
                           [False, True]],
-        pushed_blocks_footprints={1: [[False, True],
-                                      [False, False]],
-                                  2: [[False, False],
-                                      [False, True]]},
+        pushed_blocks_footprints={1: [[Range(0, 0), Range(0, 1)],
+                                      [Range(0, 0), Range(0, 0)]],
+                                  2: [[Range(0, 0), Range(0, 0)],
+                                      [Range(0, 0), Range(0, 1)]]},
         pushed_blocks={1: 11, 2: 11},
-        attached_blocks_footprints={1: [[True, False],
-                                        [False, False]],
-                                    2: [[False, True],
-                                        [False, True]]},
+        attached_blocks_footprints={1: [[Range(6, 9), Range(0, 0)],
+                                        [Range(0, 0), Range(0, 0)]],
+                                    2: [[Range(0, 0), Range(3, 6, exceptions={4})],
+                                        [Range(0, 0), Range(3, 6)]]},
         attached_blocks={1: 6, 2: 1},
     )
     SINGLE_COLUMN_PUSHER = _FlyingMachineDP.new(
@@ -134,10 +144,10 @@ class FlyingMachineEnum(Enum):
                           [True],
                           [False],
                           [True]],
-        attached_blocks_footprints={1: [[True],
-                                        [False],
-                                        [False],
-                                        [False]]},  # It can technically have attachments here, but doing so is stupid
+        attached_blocks_footprints={1: [[Range(6, 10)],
+                                        [Range(0, 0)],
+                                        [Range(0, 0)],
+                                        [Range(0, 0)]]},  # It can technically have attachments here, but that is stupid
         attached_blocks={1: 2},
     )
     SINGLE_COLUMN_PUSHER_SIDEWAYS = _FlyingMachineDP.new(
@@ -149,7 +159,7 @@ class FlyingMachineEnum(Enum):
         trigger_delay=6,
         engine_footprint=[[False, True, True],
                           [True, False, False]],
-        attached_blocks_footprints={1: [[False, False, True],
-                                        [False, False, False]]},
+        attached_blocks_footprints={1: [[Range(0, 0), Range(0, 0), Range(6, 10)],
+                                        [Range(0, 0), Range(0, 0), Range(0, 0)]]},
         attached_blocks={1: 2},
     )
